9803a8507c49400e62586892c2c076448a10226a
Anup Malleswarapu <132218710+mvsanup17@users.noreply.github.com>
2025-04-05 18:19:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://book-finder-mern.netlify.app/'
+}));
 app.use(express.json());
 
 const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
